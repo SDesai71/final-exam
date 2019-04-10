9b55ee3b705165cd29a108708a27728e713e13e3
Sahil Desai <sdesai10@my.bcit.ca>
2019-04-10 12:05:15 -0700

    views

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express');
 var app = express();
 const port = process.env.PORT || 8080;
 
+app.set('view engine','hbs');
+app.use(express.static(__dirname+'/views'));
+
+
 
 app.get('/',(request,response)=>{
     response.render('form.hbs')
