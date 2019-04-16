6a96e9f19d2b02c8c9b09396ddd4faaa8a241378
Sahil Desai <sdesai10@my.bcit.ca>
2019-04-16 02:19:31 -0700

    changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const hbs = require('hbs');
 const express = require('express');
 var app = express();
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 8080;
 
 app.set('view engine','hbs');
@@ -12,6 +13,17 @@ app.get('/',(request,response)=>{
     response.render('form.hbs')
 });
 
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(bodyParser.urlencoded());
+
+// Parse JSON bodies (as sent by API clients)
+app.use(bodyParser.json());
+
+// Access the parse results as request.body
+app.post('/', function(request, response){
+    console.log(request.body.Username);
+    console.log(request.body.Password);
+});
 
 app.listen(port,() =>{
    console.log((`server is up and listing on port ${port}`))
